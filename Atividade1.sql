c89c6f892d544add534fc04c5eba70c77bf2e060
Lucas P Monteiro <lucaspmonteiro220@gmail.com>
2025-04-17 23:41:59 -0300

    Atividade1.sql

--- Atividade1.sql
+++ Atividade1.sql
@@ -1,20 +1,20 @@
 create database sistemaAcademico;
 use sistemaAcademico;
 create table CURSO(
-	CODCURSO char(3) not null,
+    CODCURSO char(3) not null,
     NOME char(30),
     MENSALIDADE decimal(6,2),
 	primary key(CODCURSO)
 );
 create table ALUNO(
-	RA char(9) not null primary key,
+    RA char(9) not null primary key,
     RG char(9) not null,
     NOME char(30),
     CODCURSO char(3),
     foreign key (CODCURSO) references CURSO (CODCURSO)
 );
 create table DISCIPLINA(
-	CodDisc char(5) not null,
+    CodDisc char(5) not null,
     Nome char(30),
     CodCurso char(3),
     NroCreditos int,
@@ -22,7 +22,7 @@ create table DISCIPLINA(
     foreign key (CodCurso) references CURSO(codcurso)
 );
 create table BOLETIM(
-	Ra char(9) not null,
+    Ra char(9) not null,
     CodDisc char(5) not null,
     Nota decimal(5,2),
     primary key (Ra, CodDisc),
@@ -30,20 +30,19 @@ create table BOLETIM(
     foreign key (CodDisc) references DISCIPLINA (CodDisc)
 );
 insert into 
-	CURSO (CODCURSO, NOME, MENSALIDADE)
+    CURSO (CODCURSO, NOME, MENSALIDADE)
     values 
     ('AS','ANALISE DE SISTEMAS',1000.00),
-	('CC','CIENCIA DA COMPUTAÇÃO',950.00),
+    ('CC','CIENCIA DA COMPUTAÇÃO',950.00),
     ('SI','SISTEMAS DE INFORMACAO',800.00);
 
-insert into 
-	ALUNO (RA,RG,NOME,CODCURSO)
+insert into ALUNO (RA,RG,NOME,CODCURSO)
     values 
     ('123','12345','BIANCA MARIA PEDROSA','AS'),
     ('212','21234','TATIANE CITTON','AS'),
     ('221','22145','ALEXANDRE PEDROSA','CC'),
     ('231','23144','ALEXANDRE MONTEIRO','CC'),
-	('321','32111','MARCIA RIBEIRO','CC'),
+    ('321','32111','MARCIA RIBEIRO','CC'),
     ('661','66123','JUSSARA MARANDOLA','SI'),
     ('765','76512','WALTER RODRIGUES','SI');
     
